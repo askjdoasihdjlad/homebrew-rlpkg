6dd101301eba9e57add3c7cc43cde111f6c0101e
askjdoasihdjlad <qqa23581@gmail.com>
2026-05-07 17:44:08 +0200

    Update rlpkg.rb

--- Formula/rlpkg.rb
+++ Formula/rlpkg.rb
@@ -5,7 +5,7 @@ class Rlpkg < Formula
   sha256 "33a364c75b537335e1f6ee80078b0d8552e5eab55302a615d1033c67df5d005c"
 
   def install
-    bin.install Dir["rlpkg/*"]
+    bin.install "rlpkg/rlpkg"
   end
 
   test do
