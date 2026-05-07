1155908e1573d86f1e1b8dace7e32f96deb29db6
askjdoasihdjlad <qqa23581@gmail.com>
2026-05-07 17:42:19 +0200

    Update rlpkg.rb

--- Formula/rlpkg.rb
+++ Formula/rlpkg.rb
@@ -2,7 +2,7 @@ class Rlpkg < Formula
   desc "Description of rlpkg"
   homepage "https://github.com/askjdoasihdjlad/rlpkg"
   url "https://github.com/askjdoasihdjlad/rlpkg/archive/refs/tags/rlpkg1.zip"
-  sha256 "5714cafad29cbb0dfdad21f2342e62c2b6b326f23bafc75fc0559f9ae730d566"
+  sha256 "33a364c75b537335e1f6ee80078b0d8552e5eab55302a615d1033c67df5d005c"
 
   def install
     bin.install Dir["rlpkg/*"]
