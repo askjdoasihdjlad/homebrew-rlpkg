d072d0a6c4080eb87869d9a4b622b92c0913ceb5
askjdoasihdjlad <qqa23581@gmail.com>
2026-05-07 18:06:03 +0200

    Update rlpkg.rb

--- Formula/rlpkg.rb
+++ Formula/rlpkg.rb
@@ -1,15 +1,18 @@
 class Rlpkg < Formula
-  desc "Description of rlpkg"
+  desc "rlpkg"
   homepage "https://github.com/askjdoasihdjlad/rlpkg"
-  url "https://github.com/askjdoasihdjlad/rlpkg/releases/download/rlpkg1/rlpkg.tar.gz"
-  version "1.0.0"
-  sha256 "5714cafad29cbb0dfdad21f2342e62c2b6b326f23bafc75fc0559f9ae730d566"
+  url "https://github.com/askjdoasihdjlad/rlpkg/archive/refs/tags/rlpkg-r.tar.gz"
+  version "2.0.0"
+  sha256 "7d86d81d26e3d0b86960386a4a943629bf213349390e5b41fbf74d005f5e5f88"
+
+  depends_on "python@3"
 
   def install
-    libexec.install Dir["rlpkg/*"]
+    # This moves all files from the GitHub-generated folder to libexec
+    libexec.install Dir["rlpkg-rlpkg-r/*"]
   end
 
   test do
-    assert_match "python", shell_output("#{bin}/rlpkg --help", 1)
+    system "#{bin}/rlpkg", "--version"
   end
 end
